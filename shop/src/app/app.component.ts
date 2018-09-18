454414a0efe383e3b4582674734c828484473317
Ivan Khud <Ivan_Khud@epam.com>
2018-09-18 17:53:25 +0300

    added products list

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Products } from './products';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop';
+  products = Products;
 }
---
+++ shop/src/app/products.ts
@@ -0,0 +1,23 @@
+export const Products = [
+    {
+        name: 'some device',
+        description: 'description1',
+        price: 2,
+        category: 'Electronics',
+        isAvailable: true,
+    },
+    {
+        name: 'some toy',
+        description: 'description2',
+        price: 4,
+        category: 'Toys',
+        isAvailable: false,
+    },
+    {
+        name: 'some clothing',
+        description: 'description3',
+        price: 3,
+        category: 'Clothing',
+        isAvailable: true,
+    },
+]
