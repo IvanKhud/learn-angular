ffd016752096d5a337922d71835e9f72f34460f2
Ivan Khud <Ivan_Khud@epam.com>
2018-09-24 17:05:36 +0300

    pulled changes from Task1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListService } from './product-list/product-list.service';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductListComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductListService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
