615bc2f9caa151bc530bb369e1f80fd9859677f5
Ivan Khud <Ivan_Khud@epam.com>
2018-09-26 18:16:21 +0300

    small style changes

--- src/app/core/cart-list/cart-list.component.ts
+++ src/app/core/cart-list/cart-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product, CartItem } from '../../shared/models/product.model';
+import { CartItem } from '../../shared/models/product.model';
 
 import { CartService } from '../../shared/services/cart.service';
 
--- src/app/shared/services/product-list.service.ts
+++ src/app/shared/services/product-list.service.ts
@@ -17,6 +17,8 @@ export class ProductListService {
       new Product('first product', 'some description', 2, Category.Electronics, true, '1'),
       new Product('second product', 'some description', 3, Category.Toys, false, '2'),
       new Product('third product', 'some description', 1, Category.Clothing, true, '3'),
+      new Product('fourth product', 'some description', 5, Category.Toys, true, '4'),
+      new Product('fifth product', 'some description', 4, Category.Clothing, true, '5'),
     ];
   }
 
