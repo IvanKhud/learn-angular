9efabc8403d2628e17ffb9ca59cd77a0c1088e3a
Ivan Khud <Ivan_Khud@epam.com>
2018-10-02 17:05:09 +0300

    added injectable to configservice

--- src/app/shared/services/config-options.service.ts
+++ src/app/shared/services/config-options.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 
+@Injectable()
 export class ConfigOptionsService {
 
     constructor(private user: User) {}
