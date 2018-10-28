1f4c15282dfefbce1137d51864a3f071463ef02b
Ivan Khud <anakin16@gmail.com>
2018-10-28 19:02:38 +0200

    added app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
   declarations: [
@@ -13,8 +15,13 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     SharedModule,
     CoreModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+    console.log(JSON.stringify(router.config, undefined, 2));
+  }
+}
